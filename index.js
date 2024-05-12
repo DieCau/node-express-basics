239c3cb24756f1c7a0769bc0af8ca15fdfff2694
Diego Caucota <diegocaucota@gmail.com>
2024-05-12 16:25:13 -0300

    Crud parcial progress

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 console.clear();
-
 import express from 'express';
+import dotenv from 'dotenv';
+import { USERS_BBDD } from './bbdd.js'
+
 const app = express();
 
-const PORT = 3000
+dotenv.config();
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.text());
 
-app.post('/micuenta', (req, res) => {
-  console.log(req.body);  
-  res.send();
+// Obtener los detalles de una cuenta
+app.get('/account/:guid', (req,res) => {
+    // obtener un usuario
+
+    const user = USERS_BBDD.find(user => user.guid === req.params.guid)
+    
+    if( !user ) res.status(404).send()
+
+    res.send(user);
+    
 });
 
-app.put('/producto', (req, res) => {
-  console.log(req.body);  
-  res.send();
-});
+// Crear una cuenta
+app.post('/account', (req,res) => {});
+
+// Actualizar una cuenta
+app.path('/account', (req,res) => {});
+
+// Eliminar una cuenta
+app.delete('/account', (req,res) => {});
+
 
 app.listen(PORT, () => console.log(`Servidor escuchando en puerto ${PORT}`));
